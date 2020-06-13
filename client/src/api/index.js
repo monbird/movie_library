d676f0c67d6a6e102e73d49be943bc38d9b1aadb
Monika Ptaszynska <mon.ptasz@gmail.com>
2020-06-13 14:15:25 +0100

    read database base url from .env

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3018/api',
+    baseURL: process.env.REACT_APP_API_BASE_URL,
 })
 
 export const createMovieOrSeries = (payload) => api.post('/user/5eca669c7ae575ae9831c4bb/movie', payload);
